8ed4eed4c893a57589d76ccd8033632f585d7bd5
Jose Pedro santos <zepedroabsantos@gmail.com>
2023-09-22 18:03:43 +0100

    lesson 15 start

--- js_review/script.js
+++ js_review/script.js
@@ -219,6 +219,7 @@ function getTotalReviewCount(book) {
 console.log(getTotalReviewCount(book));
 */
 
+/*
 function getTotalReviewCount(book) {
   const goodreads = book.reviews?.goodreads?.reviewsCount ?? 0;
   const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
@@ -278,3 +279,9 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
   book.id === 1 ? { ...book, pages: 1210 } : book
 );
 booksAfterUpdate;
+*/
+
+fetch('https://jsonplaceholder.typicode.com/todos/1').then(res=>res.json()).then(data=>console.log(data));
+
+console.log("jonas");
+
