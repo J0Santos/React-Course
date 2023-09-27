348588025c9efdf9e8b193d23b638beaffbf5a61
Jose Pedro santos <zepedroabsantos@gmail.com>
2023-09-27 19:35:13 +0100

    Challenge after revision - Thought the logic was a different one, small changes made

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -14,8 +14,7 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function handleStepMinus() {
-		if (step > 1)
-    	setStep((s) => s - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleStepPlus() {
@@ -23,50 +22,57 @@ function Counter() {
   }
 
   function handleCountMinus() {
-    setCount((s) => s - 1);
+    setCount((c) => c - step);
   }
 
   function handleCountPlus() {
-    setCount((s) => s + 1);
+    setCount((c) => c + step);
   }
 
   const date = new Date();
-	const displayDate = new Date(date.setDate(date.getDate() + count * step))
+  const displayDate = new Date(date.setDate(date.getDate() + count));
 
-  let preDate = handlePreDate();
-  function handlePreDate() {
-    let str = "";
-    if (count > 0) {
-      str = (count * step) + " days from today is ";
-    } else if (count < 0) {
-      str = (count * step) * -1 + " days ago was ";
-    } else {
-      str = "Today is ";
-    }
-    return str;
-  }
+  // let preDate = handlePreDate();
+  // function handlePreDate() {
+  //   let str = "";
+  //   if (count > 0) {
+  //     str = count * step + " days from today is ";
+  //   } else if (count < 0) {
+  //     str = count * step * -1 + " days ago was ";
+  //   } else {
+  //     str = "Today is ";
+  //   }
+  //   return str;
+  // }
 
   return (
     <div>
       <div>
         <button onClick={handleStepMinus}>-</button>
-        <p>Step: {step}</p>
+        <span>Step: {step}</span>
         <button onClick={handleStepPlus}>+</button>
       </div>
 
       <div>
         <button onClick={handleCountMinus}>-</button>
-        <p>Count: {count}</p>
+        <span>Count: {count}</span>
         <button onClick={handleCountPlus}>+</button>
       </div>
       <p>
-        {preDate}
+        {/* {preDate} */}
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${count} days ago was `}
+        </span>
         {displayDate.toLocaleString("en-US", {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  })}
+          weekday: "short",
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        })}
       </p>
     </div>
   );
