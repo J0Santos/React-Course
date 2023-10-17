6cceafe9e906ce0ee9cf84b83a830d198ca79a71
J0Santos <zepedroabsantos@gmail.com>
2023-10-17 23:53:10 +0100

    starting excercise 2 of accordion

--- 04-steps/src/App_v1.js
+++ 04-steps/src/App_v1.js
@@ -43,48 +43,25 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <StepMessage step={step}>
-            {messages[step - 1]}
-            <div className="buttons">
-              <Button
-                textColor="#aaa"
-                bgColor="#3b4cca"
-                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-              >
-                Learn now! 📚
-              </Button>
-            </div>
-          </StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
-              <span>👈</span>Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <p className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </p>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
