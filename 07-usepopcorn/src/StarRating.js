6da0a2247701b9a44489a3d49d2765a9ef99ebed
J0Santos <zepedroabsantos@gmail.com>
2023-10-26 22:39:22 +0100

    Starting coding challenge text expander

--- 07-usepopcorn/src/StarRating.js
+++ 07-usepopcorn/src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,28 +11,38 @@ const starContainerStyle = {
   display: "flex",
 };
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 48,
-	className = '',
-	messages = [],
-	defaultRating = 0,
-	onSetRating
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
 }) {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
-		onSetRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
     lineHeight: "1",
     margin: "0",
-		color,
-		fontSize: `${size / 1.5}px`
+    color,
+    fontSize: `${size / 1.5}px`,
   };
 
   return (
@@ -44,23 +55,27 @@ export default function StarRating({
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
-						color={color}
-						size={size}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
 function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
-	const starStyle = {
-		width: `${size}px`,
-		height: `${size}px`,
-		display: "block",
-		cursor: "pointer",
-	};
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
 
   return (
     <span
